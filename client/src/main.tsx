34cb3ae291c3eacddc6cdf25b3f653a58e0c3878
Aishwarya623 <paishwarya623.com>
2025-03-19 21:12:33 +0530

    Updated main.tsx and fixed line endings

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Use BrowserRouter with basename for GitHub Pages
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename="/aishu_nova"> {/* Replace with your repo name */}
+    <App />
+  </BrowserRouter>
+);
